07fd1a4877feab68f4b72510f2ee6c7c300ee710
DEYMER ANTONIO HERNANDEZ TORRRES <dhernant@contratista.epm.co>
2022-06-01 20:05:10 -0500

    includes

---
+++ server/includes.js
@@ -0,0 +1,6 @@
+const pets = ['cat', 'dog', 'bat'];
+const response = pets.includes('bat');
+console.log(response);
+const text = 'Hola desde un elemento para probar el metodo includes';
+const searchInText = text.includes('p', 3);
+console.log(searchInText);
